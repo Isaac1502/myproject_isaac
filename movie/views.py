e07a8711232f39299e178ac733d189c2dfc8c968
Isaac <isaactadinagiraldo02@gmail.com>
2022-03-03 11:41:34 -0500

    avance taller práctico

--- movie/views.py
+++ movie/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to home page</h1>')
+    return render(request, 'home.html', {'name':'Isaac Tadina'})
