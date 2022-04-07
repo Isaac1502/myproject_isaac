6cc871e101c6a6e9015782e5347a759a97a8529b
Isaac <isaactadinagiraldo02@gmail.com>
2022-04-07 13:52:55 -0500

    External ip agregada

--- myproject_isaac/settings.py
+++ myproject_isaac/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6*z$@r6#tw++6lsx!1t4^6c0(%5$u5^^a^r$vkt!(z7e(c82ut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.232.221.232']
 
 
 # Application definition
