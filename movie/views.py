ff12ae184d3ca575700bb35953e2bfdd405b377a
Isaac <isaactadinagiraldo02@gmail.com>
2022-04-07 12:02:29 -0500

    formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name':'Isaac Tadina'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+
+def about(request):
+    return HttpResponse('<h1>This is the about page<h1>')
